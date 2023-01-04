d42d77f5294a0a0a1e26570f505572b832e35747
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-04 15:41:21 -0600

    config socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server); // it expects a raw http server, that's why it was necessary to refactor code
 
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New WebSocket connection');
+});
+
+server.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
-})
+});
