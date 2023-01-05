37ec1fabc19bbab669a998ae2c1a8a39f93826b7
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-05 12:31:27 -0600

    change variables names. disable buttons until event is completed.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,32 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
+
 socket.on('message', (message) => {
   console.log(message);
 });
 
-const form = document.querySelector('#message-form');
 
-form.addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  // disable
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message, (error) => {
+
+    // enable
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -20,12 +35,13 @@ form.addEventListener('submit', (e) => {
   });
 });
 
-const sendLocationBtn = document.querySelector('#send-location');
+$sendLocationButton.addEventListener('click', () => {
 
-sendLocationBtn.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser.');
   }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -35,6 +51,7 @@ sendLocationBtn.addEventListener('click', () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute('disabled');
         console.log('Location shared!');
       }
     );
