2515f486eaea05ccf4a2cd404291e3a736269723
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-04 19:32:18 -0600

    add function to capture and emit location to server

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,8 +8,24 @@ const form = document.querySelector('#message-form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  
+
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message);
+});
+
+const sendLocationBtn = document.querySelector('#send-location');
+
+sendLocationBtn.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+
 });
